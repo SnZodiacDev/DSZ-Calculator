f4916cd2937bc97dfa54fb86d0cfa879a8deda82
127ZElBanna <127zelbanna@frhsd.com>
2025-09-11 10:20:02 -0400

    Added documentation for the opening strings and the while loop to clarify the functionality of it.

--- src/Main.java
+++ src/Main.java
@@ -22,13 +22,14 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         boolean loop = true;
 
-        while (loop) {
+        while (loop) { // Loop to allow users to do another function when they completed the last function
+            // List of available functions that the users can utilize in the program
             System.out.println("Welcome Please state what function you would like to do.");
             System.out.println("1. Addition");
             System.out.println("2. Subtraction");
             System.out.println("3. Multiplication");
             System.out.println("4. Division");
-
+            // The scanner to allow the users to select the function they want to utilize
             System.out.print("Enter a function: ");
             int input = scanner.nextInt();
 
@@ -57,6 +58,8 @@ public class Main {
                 double b_ = scanner.nextDouble();
                 System.out.println(a_ + " + " + b_ + " = " + functions.addition(a_, b_));
             }
+            /* Input to allow for the looping of the calculator to see if the user wants to do another function
+            or not "n" will end the loop and "y" or anything else will continue the loop. */
             System.out.println("Would you like to make another calculation (y/n)?");
             String answer = scanner.next();
             if (answer.equals("n")) {
